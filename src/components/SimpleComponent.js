9f52e215e6325fa2f9c4a4b25196f3e31dc28207
Haloren <mckennakyler@gmail.com>
2021-03-01 19:27:18 -0700

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,39 @@
 // Code SimpleComponent Here
+import React from 'react';
+
+export default class SimpleComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            mood: 'happy'
+            // mood: '😃'
+        }
+    }
+
+    handleClick = (event) => {
+        // console.log(event, this.state.mood)
+
+        let newMood = this.state.mood
+        newMood === 'happy' ? this.setState({mood: 'sad'}) : this.setState({mood: 'happy'})
+        // newMood === '😃' ? this.setState({mood: '😞'}) : this.setState({mood: '😃'})
+    }
+
+    render() {
+        // console.log(this.state)
+        const myStyle = {
+            color: 'white',
+            backgroundColor: '#000666',
+            textAlign: 'center',
+            fontSize: '9rem',
+            padding: '1rem',
+            margin: '2rem',
+            cursor: 'pointer'
+        }
+
+        return(
+            <div style={myStyle} onClick={this.handleClick}>
+                {this.state.mood}
+            </div>
+        )
+    }
+}
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,6 @@
 // Code SimplerComponent Here
+import React from 'react';
+
+const SimplerComponent = (props) => <div onClick={props.handleClick} value="I am just happy">{console.log(props)}</div>
+
+export default SimplerComponent
